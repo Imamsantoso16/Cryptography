7eaa9a62ec3c62e5b2ebb6407f77648b6e68c1b3
imam <apple@apples-MacBook-Pro.local>
2025-01-06 21:39:50 +0700

    [update] mengubah tampilan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,22 @@
             <h5>Dashboard</h5>
         </div>
         <div class="card-body">
+            <h5>Hi, Welcome Administrator</h5>
+        </div>
+
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="w-full d-flex justify-content-center align-items-center">
+                <img class="mb-4" style="object-fit: contain;" src="/gbe-logo.png" alt="gbe logo" width="100%"
+                    height="200px">
+            </div>
+            <p><b>The Building Management and Solutions</b></p>
+            <p>
+                PT. Gedung Bank Exim merupakan Perusahaan swasta nasional yang bergerak di bidang Pengelolaan
+                Gedung,
+                Konstruksi, Pengelolaan Residensial, Pengelolaan Air Limbah Gedung, Penyewaan Ruang Kantor dan
+                Sumberdaya Manusia</p>
         </div>
     </div>
 </div>
